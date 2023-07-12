e0c7c5e8130f3f162e23f1490a7b03eb8c07fb7e
Angela-coello <angeladarko32@gmail.com>
2023-07-12 11:10:48 +0200

    viking

--- src/soldier/Soldier.js
+++ src/soldier/Soldier.js
@@ -1,3 +1,18 @@
-class Soldier {}
+class Soldier {
+    constructor(health, strenght) {
+        this.health = health;
+        this.strength = strenght;
+    }
+  attack() {
+return this.strength;
+  }
+
+  receiveDamage (damage){
+    this.health = this.health - damage
+  }
+}
+ 
+ 
+
 
 module.exports = Soldier;
--- src/war/War.js
+++ src/war/War.js
@@ -1,3 +1,14 @@
-class War {}
-
+class War {
+ constructor (){
+    this.vikingArmy = [];
+    this.saxonArmy = [];
+ }
+    addViking(Viking) {
+     this.vikingArmy.push(Viking);
+    }
+    addSaxon(Saxon) {
+      this.saxonArmy.push(Saxon);
+     }
+    
+ }
 module.exports = War;
